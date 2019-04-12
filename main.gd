d8cc60690c6e4ef6439479762c221580f5abdd62
Mikael <prizelive@live.dk>
2019-04-12 17:49:58 +0200

    Added setter and getter for towers and targets

--- main.gd
+++ main.gd
@@ -10,6 +10,9 @@ onready var end_pos = get_node("end_pos").get_position()
 onready var nav = get_node("nav")
 onready var map = get_node("nav/map")
 
+var targets = [] setget set_targets, get_targets
+var towers = [] setget set_towers, get_towers
+
 func _ready():
 	connect("new_ghost_tower", self, "_on_new_ghost_tower")
 	connect("new_tower", self, "_on_new_tower")
@@ -25,6 +28,18 @@ func _on_new_tower(type, is_ghost, pos):
 	match type:
 		"TOWER1":
 			add_child(Tower1.new(is_ghost, pos))
+
+func set_targets(ts):
+	targets = ts
+
+func get_targets():
+	return targets
+
+func set_towers(ts):
+	towers = ts
+
+func get_towers():
+	return towers
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
